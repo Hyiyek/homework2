329d1173613428666335ab8d3d50b9411a053a70
Hyiyek <hy0013@naver.com>
2026-06-08 13:15:27 +0900

    최신

--- homework3.js
+++ homework3.js
@@ -1,7 +1,7 @@
 //printStars(maxLine) 함수를 만들고 printStars(4)를 실행했을 때 아래 모양이 출력되게 합니다. maxLine은 출력할 줄 수입니다. 오늘은 .repeat()를 쓰지 말고 반복문과 문자열 이어 붙이기로 풉니다.
 
 function printStars(maxLine){
-  for(let i = 0; i<=maxLine; i++){
+  for(let i = 0; i<=4; i++){
     let star = "";
     for(let j = 0; j<=i-1; j++){
       star = star+="*";
